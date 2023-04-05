0922132d8ecd1246f3f5df19d567dd756645e6b8
Miles Hoffman <kotan6114@gmail.com>
2023-04-05 15:48:02 -0400

    Delete Classes

--- src/Onsite.java
+++
@@ -1,5 +0,0 @@
-public class Onsite extends Order{
-    public Onsite(int item, int quantity, int method, int index) {
-        super(item, quantity, method, index);
-    }
-}
--- src/Phone.java
+++
@@ -1,5 +0,0 @@
-public class Phone extends Order{
-    public Phone(int item, int quantity, int method, int index) {
-        super(item, quantity, method, index);
-    }
-}
