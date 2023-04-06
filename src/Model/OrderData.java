6c269435df28f825948ee422f4a6ebfea41eaa46
braydenboyer <bbb5614@psu.edu>
2023-04-06 12:40:13 -0400

    start to oderdata2

--- src/Model/OrderData.java
+++ src/Model/OrderData.java
@@ -7,43 +7,61 @@ import java.util.Queue;
 
 public class OrderData {
 
-    private Queue<Order> doorDashQueue = new LinkedList<>();
-    private Queue<Order> driveThroughQueue = new LinkedList<>();
-    private Queue<Order> onSiteQueue = new LinkedList<>();
-    private Queue<Order> phoneQueue = new LinkedList<>();
+    private Queue< Order > doorDashQueue = new LinkedList<>();                        //stores all door dash orders
+    private Queue< Order > driveThroughQueue = new LinkedList<>();                    //stores all drive through orders
+    private Queue< Order > onSiteQueue = new LinkedList<>();                          //stores all onsite orders
+    private Queue< Order > phoneQueue = new LinkedList<>();                           //stores all phone orders
+    protected PriorityQueue< Order > orderPriorityQueue = new PriorityQueue<>();      //what will be cooked next
 
-    protected PriorityQueue<Order> orderPriorityQueue = new PriorityQueue<>();
 
+    private ArrayList< Order > completedOrders = new ArrayList<>();                           //where completed orders are stored
 
+    /**
+     * Adds a door dash order to doorDashQueue
+     * @param doorDashOrder Order
+     */
     public void setDoorDashQueue( Order doorDashOrder ){
         this.doorDashQueue.add( doorDashOrder );
     }
 
+    /**
+     * Adds a drivethrough order to the driveThroughQueue
+     * @param driveThroughOrder Order
+     */
     public void setDriveThroughQueue( Order driveThroughOrder ){
         this.driveThroughQueue.add( driveThroughOrder );
     }
 
     /**
-     *
-     * @param onSiteOrder
+     * Adds a onsite order to the onSiteOrderQueue
+     * @param onSiteOrder Order
      */
     public void setOnSiteQueue( Order onSiteOrder ){
         this.onSiteQueue.add( onSiteOrder );
     }
 
     /**
-     * Adds a phpne order to the phoneOrderQueue
+     * Adds a phone order to the phoneOrderQueue
      * @param phoneOrder Order
      */
     public void setPhoneQueue( Order phoneOrder ){
-        this.setPhoneQueue( phoneOrder );
+        this.phoneQueue.add( phoneOrder );
     }
 
+
+    /**
+     * Updates the orders that are next to be cooked
+     * @param sortedOrder Order
+     */
+    public void setOrderPriorityQueue( Order sortedOrder ){
+        this.setOrderPriorityQueue( sortedOrder );
+    }
     /**
      * Displays the next order to be cooked
      */
     public void getNextOrder(){
         orderPriorityQueue.peek();
     }
+
 
 }
