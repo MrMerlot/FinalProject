4687c3af0d8af7817d46bfb9e097482172dad3c6
DanielEdinger <dme5395@psu.edu>
2023-04-06 11:12:53 -0400

    .Phone

--- src/Model/Phone.java
+++ src/Model/Phone.java
@@ -1,4 +1,10 @@
 package Model;
 
-public class Phone {
+public class Phone extends Order{
+
+    protected String phoneNumber;
+    public Phone(String name) {
+        super(name);
+        super.orderType = 3;
+    }
 }
