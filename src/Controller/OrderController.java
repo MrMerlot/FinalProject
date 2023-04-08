540e5b05e9f6ad2f47fb1a8b64eae9bb6f596413
Brayden <braydenboyer@yahoo.com>
2023-04-08 17:48:05 -0400

    Updated cookView

--- src/Controller/OrderController.java
+++ src/Controller/OrderController.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class OrderController {
-    CustomerView cv = new CustomerView();
+    CustomerView cv;
     public OrderController( CustomerView cv ){
         this.cv = cv;
     }
@@ -16,39 +16,39 @@ public class OrderController {
      *
      * @throws IOException
      */
-    public void createOrder() throws IOException {
-        if(getOrderType() == 1) {
-            DoorDash order = new DoorDash(getName(), getOrderNum());
-        }
-        else if(getOrderType() == 2){
-            DriveThrough order = new DriveThrough(getName(), getOrderNum());
-        }
-        else if(getOrderType() == 3){
-            Onsite order = new Onsite(getName(), getOrderNum());
-        }
-        else{
-            Phone order = new Phone(getName(), getOrderNum());
-        }
-    }
-
-
-    public int getOrderNum(){
-        return cv.orderNum;
-    }
-    public ArrayList<Integer> getItemID(){
-        return cv.itemID;
-    }
-    public ArrayList<Integer> getQuantity(){
-        return cv.quantity;
-    }
-    public int getOrderType(){
-        return cv.orderType;
-    }
-    public String getOrderInfo(){
-        return cv.orderInfo;
-    }
-    public String getName(){
-        return cv.name;
-    }
+//    public void createOrder() throws IOException {
+//        if(getOrderType() == 1) {
+//            DoorDash order = new DoorDash(getName(), getOrderNum());
+//        }
+//        else if(getOrderType() == 2){
+//            DriveThrough order = new DriveThrough(getName(), getOrderNum());
+//        }
+//        else if(getOrderType() == 3){
+//            Onsite order = new Onsite(getName(), getOrderNum());
+//        }
+//        else{
+//            Phone order = new Phone(getName(), getOrderNum());
+//        }
+//    }
+//
+//
+//    public int getOrderNum(){
+//        return cv.orderNum;
+//    }
+//    public ArrayList<Integer> getItemID(){
+//        return cv.itemID;
+//    }
+//    public ArrayList<Integer> getQuantity(){
+//        return cv.quantity;
+//    }
+//    public int getOrderType(){
+//        return cv.orderType;
+//    }
+//    public String getOrderInfo(){
+//        return cv.orderInfo;
+//    }
+//    public String getName(){
+//        return cv.name;
+//    }
 
 }
