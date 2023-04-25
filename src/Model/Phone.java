f5ad0fd001b643d1a8ad53827d6b69676a9b7518
Miles Hoffman <kotan6114@gmail.com>
2023-04-25 18:11:15 -0400

    Can now add phone

--- src/Model/Phone.java
+++ src/Model/Phone.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 public class Phone extends Order{
 
     protected String phoneNumber;
-    public Phone(String name, int orderNumber) {
+    public Phone(String name, int orderNumber, String phone ) {
         super(name,orderNumber, 3);
+        phoneNumber = phone;
     }
 }
