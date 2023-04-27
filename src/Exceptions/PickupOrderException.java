fd75405713869d4a11d06a8171fc071adb499626
braydenboyer <bbb5614@psu.edu>
2023-04-27 16:12:49 -0400

    PickupOrder Exception Complete

--- src/Exceptions/PickupOrderException.java
+++ src/Exceptions/PickupOrderException.java
@@ -3,6 +3,6 @@ package Exceptions;
 public class PickupOrderException extends Exception{
     public PickupOrderException(){
         super();
-        System.out.println( "No selected pickup order" );
+        System.out.println( "No pickup orders or no order selected" );
     }
 }
