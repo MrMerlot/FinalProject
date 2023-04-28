1d9b7fd7bfbc62933b845c12a3c40894acfd220e
Miles Hoffman <kotan6114@gmail.com>
2023-04-28 15:51:07 -0400

    Exception label popup

--- src/Exceptions/AddToOrderException.java
+++ src/Exceptions/AddToOrderException.java
@@ -5,7 +5,6 @@ import View.CustomerView;
 public class AddToOrderException extends Exception{
     public AddToOrderException(){
         super();
-        CustomerView.setExceptionLabel("Cannot be added to order due to invalid selection");
         System.out.println("Cannot be added to order due to invalid selection");
 
     }
